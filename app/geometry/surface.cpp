9c518288733818b351db5b7bc2591cfc2dca7bd6
Morten Bendiksen <morten.bendiksen@gmail.com>
2011-11-03 13:17:27 +0100

    Removed unused constructor

--- app/geometry/surface.cpp
+++ app/geometry/surface.cpp
@@ -1,19 +1,6 @@
 #include "surface.h"
 
-Surface::Surface(QVector<Spline*>& splines, QVector4D &color)
-{
-    Spline& spline = *splines[0];
-    for (int i = 1; i< spline.length()/2; ++i) {
-        Vector3& p1 = spline.points[i];
-        Vector3& p2 = spline.points[i+1];
-        Vector3& p3 = spline.points[spline.length() -i];
-        Vector3& p4 = spline.points[spline.length() -i-1];
-        Vector3 n1 = (p1-p2)*(p1-p3);
-        Vector3 n2 = (p2-p3)*(p2-p4);
-        triangles.append(triangle(p1, p2,p3, color));
-        triangles.append(triangle(p2, p4,p3, color));
-    }
-}
+
 
 Surface::Surface(QVector<Vector3> & triangleses, QVector<Vector3> & outline, QVector4D & color) {
     for (int i = 0; i<triangleses.size(); i+=3) {
--- app/geometry/surface.h
+++ app/geometry/surface.h
@@ -11,7 +11,6 @@ class Surface : public Shape
 public:
     /** takes a list of vertices that reprecent trinagles of surfaces*/
     Surface(QVector<Vector3> & triangles, QVector<Vector3> & outline, QVector4D &color);
-    Surface(QVector<Spline*>& splines, QVector4D &color);
 };
 
 #endif // SURFACE_H
